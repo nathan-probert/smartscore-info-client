98e54078e7240aafd2ec4e4b91a5e260ea105ba9
Nathan Probert <109328434+nathan-probert@users.noreply.github.com>
2024-11-02 00:22:16 -0400

    Add ppg and home or away (#1)

--- smartscore_info_client/schemas/team_info.py
+++ smartscore_info_client/schemas/team_info.py
@@ -6,62 +6,81 @@ import ctypes
 
 
 class TeamInfoC(ctypes.Structure):
-  _fields_ = [
-    ("id", ctypes.c_int),
-    ("tgpg", ctypes.c_float),
-    ("otga", ctypes.c_float),
-  ]
+    _fields_ = [
+        ("id", ctypes.c_int),
+        ("tgpg", ctypes.c_float),
+        ("otga", ctypes.c_float),
+    ]
 
 
 @dataclass(frozen=True)
 class TeamInfo:
-  team_name: str
-  team_abbr: str
-  season: str
-  team_id: int
-  opponent_id: int
+    team_name: str
+    team_abbr: str
+    season: str
+    team_id: int
+    opponent_id: int
+    home: bool
 
-  tgpg: float = 0.0
-  otga: float = 0.0
+    tgpg: float = 0.0  # team goals per game
+    otga: float = 0.0  # other team goals against
+    otshga: float = 0.0  # other team shorthanded goals per game
 
-  _class_data_summary: dict = field(init=False, default=None, repr=False)
+    _class_data_summary: dict = field(init=False, default=None, repr=False)
+    _class_data_penalties: dict = field(init=False, default=None, repr=False)
 
-  def __post_init__(self):
-    if TeamInfo._class_data_summary is None:
-      URL = f"https://api.nhle.com/stats/rest/en/team/summary?cayenneExp=seasonId={self.season}%20and%20gameTypeId=2"
-      TeamInfo._class_data_summary = requests.get(URL, timeout=10).json()
+    def __post_init__(self):
+        if TeamInfo._class_data_summary is None:
+            URL = f"https://api.nhle.com/stats/rest/en/team/summary?cayenneExp=seasonId={self.season}%20and%20gameTypeId=2"
+            TeamInfo._class_data_summary = requests.get(URL, timeout=3).json()
 
-    object.__setattr__(
-      self, "tgpg", get_tgpg(TeamInfo._class_data_summary, self.team_id)
-    )
-    object.__setattr__(
-      self, "otga", get_otga(TeamInfo._class_data_summary, self.opponent_id)
-    )
+        if TeamInfo._class_data_penalties is None:
+            URL = f"https://api.nhle.com/stats/rest/en/team/penaltykilltime?cayenneExp=seasonId={self.season}%20and%20gameTypeId=2"
+            TeamInfo._class_data_penalties = requests.get(URL, timeout=3).json()
+
+        object.__setattr__(
+            self, "tgpg", get_tgpg(TeamInfo._class_data_summary, self.team_id)
+        )
+        object.__setattr__(
+            self, "otga", get_otga(TeamInfo._class_data_summary, self.opponent_id)
+        )
+        object.__setattr__(
+            self, "otshga", get_otshga(TeamInfo._class_data_penalties, self.opponent_id)
+        )
 
 
 class TeamInfoSchema(Schema):
-  team_name = fields.Str()
-  team_abbr = fields.Str()
-  season = fields.Str()
-  team_id = fields.Int()
-  opponent_id = fields.Int()
+    team_name = fields.Str()
+    team_abbr = fields.Str()
+    season = fields.Str()
+    team_id = fields.Int()
+    opponent_id = fields.Int()
 
-  tgpg = fields.Float()
-  otga = fields.Float()
+    tgpg = fields.Float()
+    otga = fields.Float()
+    otshga = fields.Float()
+    home = fields.Bool()
 
 
 TEAM_INFO_SCHEMA = TeamInfoSchema()
 
 
 def get_tgpg(_data, team_id):
-  for team in _data["data"]:
-    if team["teamId"] == team_id:
-      return team["goalsForPerGame"]
-  return 0.0
+    for team in _data["data"]:
+        if team["teamId"] == team_id:
+            return team["goalsForPerGame"]
+    return 0.0
 
 
 def get_otga(_data, opponent_id):
-  for team in _data["data"]:
-    if team["teamId"] == opponent_id:
-      return team["goalsAgainstPerGame"]
-  return 0.0
+    for team in _data["data"]:
+        if team["teamId"] == opponent_id:
+            return team["goalsAgainstPerGame"]
+    return 0.0
+
+
+def get_otshga(_data, opponent_id):
+    for team in _data["data"]:
+        if team["teamId"] == opponent_id:
+            return team["shorthandedGoalsAgainst"] / team["gamesPlayed"]
+    return 0.0
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,39 +4,69 @@ from schemas.player_info import PlayerInfo
 
 @pytest.fixture
 def player_info_with_stats():
-  yield PlayerInfo(
-    name="John Doe",
-    id=8478402,
-    team_id=12,
-    gpg=0.5,
-    hgpg=0.55,
-    five_gpg=0.6,
-  )
+    yield PlayerInfo(
+        name="John Doe",
+        id=8478402,
+        team_id=12,
+        gpg=0.5,
+        hgpg=0.55,
+        five_gpg=0.6,
+    )
 
 
 @pytest.fixture
 def mock_teams_api_response():
-  return {
-    "data": [
-      {"teamId": 12, "goalsForPerGame": 3.2, "goalsAgainstPerGame": 2.5},
-      {"teamId": 34, "goalsForPerGame": 2.8, "goalsAgainstPerGame": 3.1},
-    ]
-  }
+    return {
+        "data": [
+            {
+                "teamId": 12,
+                "goalsForPerGame": 3.2,
+                "goalsAgainstPerGame": 2.5,
+                "shorthandedGoalsAgainst": 1,
+                "gamesPlayed": 40,
+            },
+            {
+                "teamId": 34,
+                "goalsForPerGame": 2.8,
+                "goalsAgainstPerGame": 3.1,
+                "shorthandedGoalsAgainst": 0.5,
+                "gamesPlayed": 40,
+            },
+        ]
+    }
 
 
 @pytest.fixture
 def mock_players_api_response():
-  return {
-    "seasonTotals": [
-      {"season": "20232024", "leagueAbbrev": "NHL", "goals": 20, "gamesPlayed": 40},
-      {"season": "20222023", "leagueAbbrev": "NHL", "goals": 25, "gamesPlayed": 50},
-      {"season": "20212022", "leagueAbbrev": "NHL", "goals": 30, "gamesPlayed": 60},
-    ],
-    "last5Games": [
-      {"goals": 1},
-      {"goals": 2},
-      {"goals": 0},
-      {"goals": 3},
-      {"goals": 1},
-    ],
-  }
+    return {
+        "seasonTotals": [
+            {
+                "season": "20232024",
+                "leagueAbbrev": "NHL",
+                "goals": 20,
+                "gamesPlayed": 40,
+                "powerPlayGoals": 5,
+            },
+            {
+                "season": "20222023",
+                "leagueAbbrev": "NHL",
+                "goals": 25,
+                "gamesPlayed": 50,
+                "powerPlayGoals": 7,
+            },
+            {
+                "season": "20212022",
+                "leagueAbbrev": "NHL",
+                "goals": 30,
+                "gamesPlayed": 60,
+                "powerPlayGoals": 8,
+            },
+        ],
+        "last5Games": [
+            {"goals": 1},
+            {"goals": 2},
+            {"goals": 0},
+            {"goals": 3},
+            {"goals": 1},
+        ],
+    }
