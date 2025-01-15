73c4702ebdb47323755e906cffe0e228ffe14be1
Nathan Probert <109328434+nathan-probert@users.noreply.github.com>
2025-01-14 22:57:23 -0500

    Add otshga and home as optional fields (#5)
    * add otshga and home as optional fields
    
    * remove useless comments

--- tests/unit/test_db_player_schema.py
+++ tests/unit/test_db_player_schema.py
@@ -13,6 +13,7 @@ sample_data = {
     "tgpg": 3.31,
     "otga": 2.87,
     "scored": 1,
+    "home": True,
 }
 
 
@@ -27,6 +28,7 @@ def test_player_db_info_schema_serialization():
     assert serialized_data["hgpg"] == 0.1
     assert serialized_data["five_gpg"] == 0.12
     assert serialized_data["scored"] == 1
+    assert serialized_data["home"] is True
 
 
 def test_player_db_info_initialization():
@@ -42,6 +44,7 @@ def test_player_db_info_initialization():
         tgpg=3.31,
         otga=2.87,
         scored=1,
+        home=True,
     )
 
     assert player_info._id == "621f4f4c7b3b3b0001f3b3b3"
@@ -54,6 +57,7 @@ def test_player_db_info_initialization():
     assert player_info.tgpg == 3.31
     assert player_info.otga == 2.87
     assert player_info.scored == 1
+    assert player_info.home is True
 
 
 def test_player_db_info_c_structure():
@@ -74,3 +78,26 @@ def test_player_db_info_c_structure():
     assert player_info_c.five_gpg == pytest.approx(0.12, rel=1e-6)
     assert player_info_c.tgpg == pytest.approx(3.31, rel=1e-6)
     assert player_info_c.otga == pytest.approx(2.87, rel=1e-6)
+
+
+def test_optional_fields():
+    # Test with otshga and home undefined
+    player_info = PlayerDbInfo(
+        _id="621f4f4c7b3b3b0001f3b3b3",
+        date="2024-02-16",
+        name="Jaccob Slavin",
+        id=8476958,
+        gpg=0.08,
+        hgpg=0.1,
+        five_gpg=0.12,
+        team_name="Carolina Hurricanes",
+        tgpg=3.31,
+        otga=2.87,
+        scored=1,
+    )
+    assert player_info.otshga is None
+    assert player_info.home is None
+
+    serialized_data = PlayerDbInfoSchema().dump(player_info)
+    assert serialized_data["otshga"] is None
+    assert serialized_data["home"] is None
